14ecb06ed61c75a7a000a26071a7e082175f187e
Daniel Nascimento <pdaniel.contato@gmail.com>
2023-11-19 23:48:09 -0300

    feat: add about us page

---
+++ RadRiderWebApp/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace RadRiderWebApp.Pages;
+public class AboutUsModel : PageModel
+{
+    private readonly ILogger<AboutUsModel> _logger;
+
+    public AboutUsModel(ILogger<AboutUsModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
--- RadRiderWebApp/Pages/Index.cshtml.cs
+++ RadRiderWebApp/Pages/Index.cshtml.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace RadRiderWebApp.Pages;
 
 public class IndexModel : PageModel
