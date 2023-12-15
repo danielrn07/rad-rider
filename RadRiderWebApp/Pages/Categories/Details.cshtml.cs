bf8acd8aae05e3363e73919cd29372d26767ddb6
Daniel Nascimento <pdaniel.contato@gmail.com>
2023-12-14 23:16:10 -0300

    feat: add category crud

---
+++ RadRiderWebApp/Pages/Categories/Details.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RadRiderWebApp.Data;
+using RadRiderWebApp.Models;
+
+namespace RadRiderWebApp.Pages.Categories
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RadRiderWebApp.Data.SkateDbContext _context;
+
+        public DetailsModel(RadRiderWebApp.Data.SkateDbContext context)
+        {
+            _context = context;
+        }
+
+      public Category Category { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Category == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Category = category;
+            }
+            return Page();
+        }
+    }
+}
---
+++ RadRiderWebApp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RadRiderWebApp.Data;
+using RadRiderWebApp.Models;
+
+namespace RadRiderWebApp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RadRiderWebApp.Data.SkateDbContext _context;
+
+        public IndexModel(RadRiderWebApp.Data.SkateDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Category != null)
+            {
+                Category = await _context.Category.ToListAsync();
+            }
+        }
+    }
+}
